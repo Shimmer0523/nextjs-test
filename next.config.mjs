6894a9867af1ef0a3162d2f9c65f51ff2c68c2fd
Shimmer0523 <AndroCoffee@gmail.com>
2024-08-12 05:27:42 +0000

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/nextjs-test",
+    assetPrefix: "/nextjs-test/",
+    output: "export",
+};
+
+export default nextConfig;
