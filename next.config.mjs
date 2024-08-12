d8e938990e8d2684b5803873319aa3b2f2efa436
Shimmer0523 <AndroCoffee@gmail.com>
2024-08-12 07:28:58 +0000

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const basePath = process.env.REPO_NAME ? `/${process.env.REPO_NAME}` : "";
 const assetPrefix = process.env.REPO_NAME ? `/${process.env.REPO_NAME}` : "";
 
+console.log(process.env.REPO_NAME);
+
 const nextConfig = {
   basePath: basePath,
   output: "export",
