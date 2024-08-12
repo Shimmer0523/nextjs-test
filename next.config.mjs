4e514a140b54f8d55a1e6b9cc014a85cbe7f2337
Shimmer0523 <AndroCoffee@gmail.com>
2024-08-12 06:26:14 +0000

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.REPO_NAME ? "/" + process.env.REPO_NAME : "";
-const assetPrefix = process.env.REPO_NAME ? "/" + process.env.REPO_NAME + "/" : "";
+const basePath = process.env.REPO_NAME ? `/${process.env.REPO_NAME}` : "";
+const assetPrefix = process.env.REPO_NAME ? `/${process.env.REPO_NAME}/` : "";
 
 const nextConfig = {
-    basePath: basePath,
-    assetPrefix: assetPrefix,
-    output: "export",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
+  output: "export",
 };
 
 export default nextConfig;
